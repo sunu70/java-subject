3ff6e84c62d46bbc7867341f08fb3b4c8c14ac17
sunu70 <83327887+sunu70@users.noreply.github.com>
2021-09-06 02:08:34 +0900

    sad

--- js/weather.js
+++ js/weather.js
@@ -3,12 +3,12 @@ const API_KEY = "090648770d5383a87e3511ba0c8b24fd";
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
         const weather = document.querySelector("#weather span:first-child");
-        const temp = document.querySelector("#weather span:nth-child(2)")
+        const temp = document.querySelector("#weather span:nth-child(2)");
         const city = document.querySelector("#weather span:last-child");
       city.innerText = data.name;
       temp.innerText = Math.ceil(data.main.temp)+"℃"
