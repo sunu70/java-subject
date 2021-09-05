c355ceb3a6275b46b0935cfb67bd1880a84450c7
sunu70 <83327887+sunu70@users.noreply.github.com>
2021-09-06 01:17:27 +0900

    sad

--- js/login.js
+++ js/login.js
@@ -1,15 +1,28 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
-const HIDDEN_CLASSNAME = "hidden";
+const loginForm = document.querySelector("#login-form")
+const inputUsername = document.querySelector("#input-username")
+const inputSubmit = document.querySelector("#input-submit")
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME = "username"
 
-
-function onLoginSubmit(event) {
+function onLoginSubmit(event){
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  const greeting = document.querySelector("#greeting")
+  const username = inputUsername.value
+  localStorage.setItem(USERNAME, username)
+  paintGreeting(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME);
+function paintGreeting(username){
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello! ${username}`
+}
+
+if(savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+  paintGreeting(savedUsername);
+}
+
